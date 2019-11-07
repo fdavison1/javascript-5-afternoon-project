70f93d30d86be39da15900597e6bc26c23e8868c
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-07 14:17:33 -0700

    user

--- constructors.js
+++ constructors.js
@@ -13,37 +13,36 @@
   Create a constructor function called CarFactory that will build car objects with make and model properties.
   The values for the make and model will come from two parameters, make and model (in that order).
 */
-
 // Code here
+function CarFactory(make, model){
+  this.make = make, 
+  this.model = model
+
+}
 
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
-
 function Employee(name, email, hireDate) {
   this.name = name;
   this.email = email;
   this.hireDate = hireDate;
 }
-
 // Do not edit the code above.
 
 /*
   Invoke the constructor function above and pass in 'Bob', 'bob@gmail.com', and '01-02-98' as arguments.
   Assign the result of the invocation to a variable called bob.
 */
-
 // Code here
+const bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
 
 ////////// PROBLEM 3 //////////
-
 // Do not edit the code below.
-
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 prius.moveCar(); // Increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); // Increments mustang' move property by 10. Returns the new move property.
-
 // Do not edit the code above.
 
 /*
@@ -55,8 +54,22 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
   You'll also need to use the 'this' keyword properly in order to make sure you're
     invoking moveCar on the right object (prius vs mustang).
 */
-
 // Code here
+function Car(make, model, year){
+  this.make = make
+  this.model = model
+  this.year = year
+  this.moveCar = function(){
+    let move = 0
+    move += 10
+    return move
+  }
+}
+
+// Car.prototype.moveCar = function(){
+//   move += 10
+//   return move
+// }
 
 ////////// PROBLEM 4 //////////
 
@@ -68,14 +81,17 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
   Find the average between the old rating and the new rating.
   Change the rating property to become this new number and return the updated rating.
 */
-
 function Movie(name, genre, rating) {
   this.name = name;
   this.genre = genre;
   this.rating = rating;
 }
-
 // Code here
+Movie.prototype.changeRating = function(num){
+  let newRating = num
+  return this.rating = (newRating + this.rating)/2
+  // return this.rating
+}
 
 ////////// PROBLEM 5 //////////
 
